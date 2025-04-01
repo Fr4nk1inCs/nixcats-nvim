5e6f7c2804b9adbaac05b4a6e54715add303e324
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-04-01 21:32:43 +0800

    fix: disable flash's semicolon and comma

--- lua/plugins/core/editor/motion.lua
+++ lua/plugins/core/editor/motion.lua
@@ -3,8 +3,14 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     vscode = true,
-    ---@type Flash.Config
-    opts = {},
+    opts = {
+      ---@type table<string, Flash.Config>
+      modes = {
+        char = {
+          keys = { "f", "F", "t", "T" },
+        },
+      },
+    },
     -- stylua: ignore
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
