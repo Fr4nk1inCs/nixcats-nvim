7d9562a010cfbe2d624d921994ecf0ce0c7c0258
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-09-16 16:26:39 +0800

    fix: disable scroll in SSH connections

--- lua/plugins/core/ui/scroll.lua
+++ lua/plugins/core/ui/scroll.lua
@@ -3,7 +3,7 @@ return {
     "folke/snacks.nvim",
     opts = {
       scroll = {
-        enabled = not vim.g.neovide,
+        enabled = not (vim.g.neovide or vim.env.SSH_TTY),
       },
     },
   },
