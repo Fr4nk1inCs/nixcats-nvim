14df1836c84d62232319da1fd74ba342a71f67a1
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-11-10 20:28:43 +0800

    fix: remove ui_select opts in fzf-lua

--- lua/plugins/core/editor/picker.lua
+++ lua/plugins/core/editor/picker.lua
@@ -29,6 +29,7 @@ return {
   {
     "ibhagwan/fzf-lua",
     cmd = "FzfLua",
+    event = "VeryLazy",
     opts = function(_, opts)
       local fzf = require("fzf-lua")
       local config = fzf.config
@@ -126,7 +127,7 @@ return {
             child_prefix = false,
           },
           code_actions = {
-            previewer = vim.fn.executable("delta") == 1 and "codeaction_native" or nil,
+            previewer = vim.fn.executable("delta") == 1 and "codeaction_native" or "codeaction",
           },
         },
       }
@@ -148,47 +149,7 @@ return {
         opts[1] = nil
       end
       require("fzf-lua").setup(opts)
-    end,
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          vim.ui.select = function(...)
-            require("lazy").load({ plugins = { "fzf-lua" } })
-            require("fzf-lua").register_ui_select(function(fzf_opts, items)
-              return vim.tbl_deep_extend("force", fzf_opts, {
-                prompt = " ",
-                winopts = {
-                  title = " " .. vim.trim((fzf_opts.prompt or "Select"):gsub("%s*:%s*$", "")) .. " ",
-                  title_pos = "center",
-                },
-              }, fzf_opts.kind == "codeaction" and {
-                winopts = {
-                  layout = "vertical",
-                  -- height is number of items minus 15 lines for the preview, with a max of 80% screen height
-                  height = math.floor(math.min(vim.o.lines * 0.8 - 16, #items + 2) + 0.5) + 16,
-                  width = 0.5,
-                  preview = not vim.tbl_isempty(vim.lsp.get_clients({ bufnr = 0, name = "vtsls" })) and {
-                    layout = "vertical",
-                    vertical = "down:15,border-top",
-                    hidden = "hidden",
-                  } or {
-                    layout = "vertical",
-                    vertical = "down:15,border-top",
-                  },
-                },
-              } or {
-                winopts = {
-                  width = 0.5,
-                  -- height is number of items, with a max of 80% screen height
-                  height = math.floor(math.min(vim.o.lines * 0.8, #items + 2) + 0.5),
-                },
-              })
-            end)
-            return vim.ui.select(...)
-          end
-        end,
-      })
+      require("fzf-lua").register_ui_select()
     end,
     keys = {
       {
