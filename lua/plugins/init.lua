0b680c5c45515a0058db3e5e6e25cd1353abe445
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-12-06 15:25:39 +0800

    fix: include all plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,8 +2,10 @@ vim.lsp.enable(LangSettings.lsps)
 
 return {
   LangSettings.lazy,
-  { import = "plugins.editor" },
-  { import = "plugins.ui" },
   { import = "plugins.coding" },
+  { import = "plugins.debugger" },
+  { import = "plugins.editor" },
+  { import = "plugins.external" },
   { import = "plugins.protocols" },
+  { import = "plugins.ui" },
 }
